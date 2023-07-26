e80d44f3faed2e65a2975d932c5a0f247124823d
Aiman Suliman <125767562+ZaymanDev@users.noreply.github.com>
2023-07-26 18:19:09 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-document.getElementById("contactBtn").addEventListener("click", function() {
-    document.getElementById("contactModal").style.display = "block";
-});
-window.addEventListener("click", function(event) {
-    var modal = document.getElementById("contactModal");
-    if (event.target == modal) {
-        modal.style.display = "none";
+const rotateImage = document.getElementById('rotateImage');
+const navBar = document.querySelector('.nav');
+const rotateButton = document.getElementById('rotateButton');
+const contact = document.getElementById('contactForm');
+const openBtn = document.getElementById('openBtn');
+const closeBtn = document.querySelector('.contact .close');
+
+let navIsOpen = false;
+
+rotateImage.addEventListener('click', function() {
+    rotateImage.classList.toggle('clicked');
+    if (!navIsOpen) {
+        navBar.classList.add('show');
+        rotateButton.style.marginLeft = '10px';
+    } else {
+        navBar.classList.remove('show');
+        rotateButton.style.marginLeft = '10px';
     }
+    navIsOpen = !navIsOpen;
 });
+
+openBtn.addEventListener('click', function () {
+    contact.classList.add('show');
+});
+
+closeBtn.addEventListener('click', function () {
+    contact.classList.remove('show');
+});
+
